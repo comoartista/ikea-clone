034ec0ff6b9d2e492530780ed508d7159e2d4022
Oksana Melnyk <oks.melnuk@gmail.com>
2023-07-16 22:43:17 +0200

    add header

--- app.js
+++ app.js
@@ -1,39 +1,87 @@
-async function fetchData() {
-  const url = 'https://ikea-api.p.rapidapi.com/keywordSearch?keyword=chair&countryCode=us&languageCode=en';
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': 'b5c9718317msh1ac1cb058c518ebp15d7a1jsn62f63efda339',
-      'X-RapidAPI-Host': 'ikea-api.p.rapidapi.com'
-    }
-  };
+// async function fetchData() {
+//   const url = 'https://ikea-api.p.rapidapi.com/keywordSearch?keyword=chair&countryCode=us&languageCode=en';
+//   const options = {
+//     method: 'GET',
+//     headers: {
+//       'X-RapidAPI-Key': 'b5c9718317msh1ac1cb058c518ebp15d7a1jsn62f63efda339',
+//       'X-RapidAPI-Host': 'ikea-api.p.rapidapi.com'
+//     }
+//   };
 
-  try {
-    const response = await fetch(url, options);
-    const result = await response.json();
-    createCard(result)
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  }
-}
-fetchData(); // Call the async function to initiate the fetching process
+//   try {
+//     const response = await fetch(url, options);
+//     const result = await response.json();
+//     createCard(result)
+//     console.log(result);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+// fetchData(); // Call the async function to initiate the fetching process
 
-const productsList = document.querySelector('.productsList')
-function createCard(products) {
-  products.forEach(product => {
-    const li = document.createElement('li')
-    li.classList.add('text')
+// const productsList = document.querySelector('.productsList')
+// function createCard(products) {
+//   products.forEach(product => {
+//     const li = document.createElement('li')
 
-    const name = document.createElement('h3')
-    name.textContent = product.name
+//     const img = document.createElement('img')
+//     img.src = product.image;
+//     img.alt = product.imageAlt
+//     img.width = '240'
 
-    const img = document.createElement('img')
-    img.src = product.image;
-    img.width = '100'
+//     img.addEventListener('mouseover', () => {
+//       img.src = product.contextualImageUrl
+//     })
+
+//     img.addEventListener('mouseout', () => {
+//       img.src = product.image;
+//     });
 
 
-    li.append(name, img)
-    productsList.append(li)
-  });
-}
+//     const name = document.createElement('h3')
+//     name.textContent = product.name
+
+//     const typeName = document.createElement('p')
+//     typeName.textContent = product.typeName
+
+//     const price = document.createElement('span')
+//     price.textContent = `${product.price.currentPrice} ${product.price.currency}`
+//     if (product.price.discounted) {
+//       price.classList.add('red')
+//     }
+
+//     const images = product.variants.map(variant => variant.image);
+//     const variants = document.createElement('div')
+
+//     images.forEach(image => {
+//       const variantImage = document.createElement('img')
+//       variantImage.src = image
+//       variantImage.width = '30'
+//       variants.append(variantImage)
+//     })
+
+    
+//     li.append(img, name, typeName, price, variants)
+//     productsList.append(li)
+//   });
+// }
+
+
+
+
+const filterBar = document.querySelector('.filter-bar')
+
+filterBar.addEventListener('click', (e) => {
+  const filterBarArrows = document.querySelectorAll('.filter-bar__arrow')
+
+  filterBarArrows.forEach(filterBarArrow => {
+    filterBarArrow.classList.remove('filter-bar__arrow--up')
+  })
+
+  const filterBarArrow = e.target.querySelector('.filter-bar__arrow')
+
+  if (filterBarArrow) {
+    filterBarArrow.classList.add('filter-bar__arrow--up')
+  } 
+})
+
